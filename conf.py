9a599fa53dea60a4549acc07c811513400a94872
Mian Lu <lumianph@gmail.com>
2022-03-13 12:44:38 +0800

    update version number in conf.py for v0.4

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ copyright = '2022, OpenMLDB'
 author = 'OpenMLDB'
 
 # The full version, including alpha/beta/rc tags
-version = 'dev'
+version = 'v0.4'
 
 
 # -- General configuration ---------------------------------------------------
